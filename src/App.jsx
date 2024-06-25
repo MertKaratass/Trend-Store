14435eab591b2b6effa94afc4cb2831aa61f6e90
“red” <karatasmert255@gmail.com>
2024-06-25 14:55:34 +0300

    chore: Add new components and styles for products, header, and hero sections

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Cart from "./Components/Cart/Cart";
+import Hero from "./Components/Hero/Hero";
+import Header from "./Components/Layout/Header";
+import Products from "./Components/Products/Products";
+
+function App() {
+  return (
+    <div>
+      <Cart />
+      <Header />
+      <Hero />
+      <Products />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Cart/Cart.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Cart = () => {
+  return (
+    <div className='offcanvas'>
+        <div className="content">
+            <div className="cart-head">
+                <h2>Sepetim</h2>
+                <a href="/">X</a>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Cart
